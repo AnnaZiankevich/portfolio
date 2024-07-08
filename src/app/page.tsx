f6853c58e8f8122e79bef2206a0ab0dc58672865
Anna Ziankevich <anna.ziankevich@gmail.com>
2024-07-08 18:25:23 +0300

    Add experience block

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,8 +14,18 @@ import sass from '../assets/sass.svg'
 import git from'../assets/git.svg'
 import aspnet from '../assets/aspnet.svg'
 import figma from '../assets/figma.svg'
+import { ExperienceCalculator } from "../helpers/helpers"
+import fintech from '../assets/fintech.png'
+import pnpl from '../assets/pineapple practice.png'
+import dr from '../assets/dom rebenka.png'
 
 export default function Home() {
+  const periods = [
+    { start: '2022-06', end: '2024-01' },
+    { start: '2023-08', end: '2024-03' },
+    { start: '2024-02', end: 'сейчас' },
+  ];
+
   const technologies = [
     {
       image: react.src,
@@ -90,7 +100,7 @@ export default function Home() {
         <div className={styles.technologies_block__title}>Технологии</div>
         <div className={styles.technologies_block__content}>
           {technologies.map(item => (
-            <div className={styles.technologies_block__item}>
+            <div className={styles.technologies_block__item} key={item.title}>
               <img src={item.image} 
                    alt={item.title}
               />
@@ -99,6 +109,102 @@ export default function Home() {
               </div>
             </div>
           ))}
+        </div>
+      </div>
+      <div className={styles.experience_block}>
+        <div className={styles.experience_block__title}>
+          <div className={styles.experience_block__title_main}>
+            Мой опыт работы
+          </div>
+          <ExperienceCalculator periods={periods} />
+        </div>
+        <div className={styles.experience_block__content}>
+          <div className={styles.experience_block__item}>
+            <div className={styles.experience_block__item_header}>
+              <div className={styles.experience_block__item_company}>
+                <a href="https://minefintech.com/ru.html">
+                  <img className={styles.company__icon} 
+                       src={fintech.src}
+                  />
+                </a>
+                <div className={styles.company__info}>
+                  <div className={styles.company__post}>Frontend developer</div>
+                  <div className={styles.company__name}>Fintech Capital and Custody LLC</div>
+                </div>
+              </div>
+              <div className={styles.experience_block__period}>
+                февраль 2024 - сейчас
+              </div>
+            </div>
+            <div className={styles.experience_block__item_content}>
+              Я занимаюсь разработкой и поддержкой веб-приложений для банков. Разрабатываю архитектуру 
+              приложений и бизнес-процессы, создаю многостраничные сайты и поддерживаю сайты на WordPress.  
+              В своей работе использую технологии React (Redux toolkit, Formik, React Router DOM, Axios, 
+              AntDesign), TypeScript, GitLab, Tailwind CSS, SCSS и WordPress.
+            </div>
+          </div>
+
+          <div className={styles.experience_block__item}>
+            <div className={styles.experience_block__item_header}>
+              <div className={styles.experience_block__item_company}>
+                <a href="https://pnpl.site/">
+                  <img className={styles.company__icon} 
+                       src={pnpl.src}
+                  />
+                </a>
+                <div className={styles.company__info}>
+                  <div className={styles.company__post}>Frontend developer</div>
+                  <div className={styles.company__name}>Pineapple Practice</div>
+                </div>
+              </div>
+              <div className={styles.experience_block__period}>
+                август 2023 - март 2024
+              </div>
+            </div>
+            <div className={styles.experience_block__item_content}>
+              В рамках проектной занятости велась работа с технологиями HTML, SCSS, React 
+              (Redux, React Hook Form, React Router DOM, Axios, Chart.js), GitLab и PostgreSQL.
+              <br/><br/>
+              Участие в создании и конкретизации технического задания, планирование задач по 
+              спринтам, а также разработка базы данных проекта. Разработка компонентов и кастомных 
+              элементов интерфейса, создание панели администратора, панели пользователя, различные 
+              дашборды, проектирование и создание бизнес-логики курсов, уроков и тестов. Для 
+              взаимодействия с backend использовала Swagger и Axios.
+              <br/><br/>
+              Ссылка на итоговый проект: <a href="https://front.education.pnpl.tech/">
+                https://front.education.pnpl.tech/
+              </a>
+            </div>
+          </div>
+
+          <div className={styles.experience_block__item}>
+            <div className={styles.experience_block__item_header}>
+              <div className={styles.experience_block__item_company}>
+                <a href="http://domrebenkaminsk.by/">
+                  <img className={styles.company__icon} 
+                       src={dr.src}
+                  />
+                </a>
+                <div className={styles.company__info}>
+                  <div className={styles.company__post}>Web developer</div>
+                  <div className={styles.company__name}>Дом ребенка № 1</div>
+                </div>
+              </div>
+              <div className={styles.experience_block__period}>
+                июнь 2022 - январь 2024
+              </div>
+            </div>
+            <div className={styles.experience_block__item_content}>
+              Создание варфреймов и дизайна в Figma, а также разработка и обновление контента. Разработка 
+              сайта с использованием HTML, CSS, PHP, JavaScript и CMS WordPress, включая натяжку верстки. 
+              После завершения разработки проводилось тестирование и запуск проекта. Кроме того, 
+              обеспечивалась поддержка и регулярное обновление сайта.
+              <br/><br/>
+              Ссылка на итоговый проект: <a href="http://domrebenkaminsk.by/">
+                http://domrebenkaminsk.by/
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </main>
--- src/helpers/helpers.tsx
+++ src/helpers/helpers.tsx
@@ -1,1 +1,52 @@
+import React from 'react';
+import { differenceInMonths, parse } from 'date-fns';
+import { log } from 'console';
+
+type Period = {
+    start: string;
+    end: string;
+};
+
+type ExperienceCalculatorProps = {
+  periods: Period[];
+};
+
 export const currentYear = new Date().getFullYear();
+
+const getDeclension = (number: number, singular: string, few: string, many: string): string => {
+    const mod10 = number % 10;
+    const mod100 = number % 100;
+  
+    if (mod10 === 1 && mod100 !== 11) {
+      return singular;
+    } else if (mod10 >= 2 && mod10 <= 4 && (mod100 < 10 || mod100 >= 20)) {
+      return few;
+    } else {
+      return many;
+    }
+};
+
+export const ExperienceCalculator: React.FC<ExperienceCalculatorProps> = ({ periods }) => {
+    const calculateTotalExperience = (periods: Period[]): number => {
+      let totalMonths = 0;
+  
+      periods.forEach(period => {
+        const startDate = parse(period.start, 'yyyy-MM', new Date());
+        const endDate = period.end === 'сейчас' ? new Date() : parse(period.end, 'yyyy-MM', new Date());
+        totalMonths += differenceInMonths(endDate, startDate);
+      });
+  
+      return totalMonths;
+    };
+  
+    const totalExperience = calculateTotalExperience(periods);
+    const years = Math.floor(totalExperience / 12);
+    const months = totalExperience % 12;
+  
+    const yearWord = getDeclension(years, 'год', 'года', 'лет');
+    const monthWord = getDeclension(months, 'месяц', 'месяца', 'месяцев');
+  
+    return (
+        <div>{years} {yearWord} {months <= 0 ? '' : `и ${months} ${monthWord}`}</div>
+    );
+};
